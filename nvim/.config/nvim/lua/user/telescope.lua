6cac7c208f890a71b22f24284b6232826ca81932
Simon Weiß <simon.weiss@alexanderthamm.com>
2022-09-13 16:34:31 +0200

    Search hidden for grep

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -12,6 +12,16 @@ telescope.setup {
     selection_caret = " ",
     path_display = { "smart" },
     file_ignore_patterns = { ".git/", "node_modules" },
+     vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--hidden'
+    },
 
     mappings = {
       i = {

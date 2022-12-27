d92143e6b2e1299a7f0a854600ed9dac0f6d1690
Simon Weiß <simon.weiss@alexanderthamm.com>
2022-12-27 16:30:41 +0100

    Add project

--- bin/.local/bin/tmux-start-fzf.sh
+++ bin/.local/bin/tmux-start-fzf.sh
@@ -42,11 +42,22 @@ if ! tmux has-session -t=$selected_name 4> /dev/null; then
     window=2
     tmux new-window -t $selected_name:$window -n "Server" -c $selected
     tmux send-keys -t $selected_name:$window "hugo server -D" C-m
+  elif [[ $selected_name == "codecrafters-redis-rust" ]]; then
+    window=0
+    tmux rename-window -t $selected_name:$window "Vim"
+    tmux send-keys -t $selected_name:$window "vim" C-m
+    window=1
+    tmux new-window -t $selected_name:$window -c $selected
+    tmux split-window -h
+    tmux split-window -v
+    tmux send-keys -t $selected_name:$window.1 "redis-server --port 6380" C-m
+    tmux send-keys -t  $selected_name:$window.2 "./spawn_redis_server.sh" C-m
   else
     window=0
     tmux send-keys -t $selected_name:$window "vim" C-m
     window=1
     tmux new-window -t $selected_name:$window -c $selected
+
   fi
 
 fi

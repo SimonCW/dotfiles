6e1220c1c2e38284358113de5506e8f37bbe0088
Simon Weiß <simon.weiss@alexanderthamm.com>
2023-02-03 18:42:55 +0100

    Don't insert directly

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -1,7 +1,7 @@
 vim.opt.clipboard = nil --"unnamedplus"               -- allows neovim to access the system clipboard
 vim.opt.backup = false -- creates a backup file
 vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
-vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
+vim.opt.completeopt = { "menuone", "noinsert", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
 vim.opt.fileencoding = "utf-8" -- the encoding written to a file
 vim.opt.hlsearch = true -- highlight all matches on previous search pattern

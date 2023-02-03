b406650282b3d746a65d2a39505817b9933fdc8b
Simon Weiß <simon.weiss@alexanderthamm.com>
2023-02-03 18:43:09 +0100

    Fix rust-tools setup. Inlay hints still not working

--- nvim/.config/nvim/lua/user/lsp/mason.lua
+++ nvim/.config/nvim/lua/user/lsp/mason.lua
@@ -6,7 +6,6 @@ local servers = {
   "bashls",
   "jsonls",
   "yamlls",
-  "rust_analyzer",
 }
 
 local settings = {
@@ -36,17 +35,49 @@ end
 local opts = {}
 
 for _, server in pairs(servers) do
-  opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
-  }
+  if server ~= "rust_analyzer" then
+    opts = {
+      on_attach = require("user.lsp.handlers").on_attach,
+      capabilities = require("user.lsp.handlers").capabilities,
+    }
+    server = vim.split(server, "@")[1]
+    local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
+    if require_ok then
+      opts = vim.tbl_deep_extend("force", conf_opts, opts)
+    end
 
-  server = vim.split(server, "@")[1]
-
-  local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
-  if require_ok then
-    opts = vim.tbl_deep_extend("force", conf_opts, opts)
+    lspconfig[server].setup(opts)
   end
-
-  lspconfig[server].setup(opts)
 end
+-- Setting up Rust tools seperately
+local ropts = {
+  tools = {
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
+  },
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+  server = {
+    capabilities = require("user.lsp.handlers").capabilities,
+    on_attach = require("user.lsp.handlers").on_attach,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+  },
+}
+require("rust-tools").setup(ropts)

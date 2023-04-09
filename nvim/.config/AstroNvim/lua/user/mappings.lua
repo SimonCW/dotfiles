92038a176c24d3d1daf3be96bc725791bfe72899
Simon Weiß <simon.weiss@alexanderthamm.com>
2023-04-09 22:53:44 +0200

    Set lsp mappings

--- nvim/.config/AstroNvim/lua/user/mappings.lua
+++ nvim/.config/AstroNvim/lua/user/mappings.lua
@@ -8,10 +8,29 @@ return {
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
+
+    -- LSP mappings
+    ["gh"] = { "<cmd>lua vim.lsp.buf.hover()<CR>" },
+    ["gD"] = { "<cmd>lua vim.lsp.buf.declaration()<CR>" },
+    ["gd"] = { "<cmd>lua vim.lsp.buf.definition()<CR>" },
+    ["gI"] = { "<cmd>lua vim.lsp.buf.implementation()<CR>" },
+    ["gr"] = { "<cmd>lua vim.lsp.buf.references()<CR>" },
+    ["gl"] = { "<cmd>lua vim.diagnostic.open_float()<CR>" },
+    ["<leader>lI"] = { "<cmd>LspInstallInfo<cr>" },
+    ["<leader>la"] = { "<cmd>lua vim.lsp.buf.code_action()<cr>" },
+    ["<leader>ls"] = { "<cmd>lua vim.lsp.buf.signature_help()<CR>" },
+    -- Navigate buffers
+    ["<S-l>"] = { ":bnext<CR>" },
+    ["<S-h>"] = { ":bprevious<CR>" },
+    ["<S-q>"] = { "<cmd>Bdelete!<CR>" },
+    -- Was here before
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
+          require("astronvim.utils.buffer").close(
+            bufnr)
+        end)
       end,
       desc = "Pick to close",
     },

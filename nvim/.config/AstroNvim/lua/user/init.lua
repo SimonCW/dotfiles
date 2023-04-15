7a3676be97e1238349c1f9d9438ea860507481c5
Simon Weiß <simon.weiss@alexanderthamm.com>
2023-04-15 21:35:31 +0200

    Enable text wrapping for markdown files

--- nvim/.config/AstroNvim/lua/user/init.lua
+++ nvim/.config/AstroNvim/lua/user/init.lua
@@ -76,5 +76,12 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    vim.api.nvim_create_autocmd({ "FileType" }, {
+      pattern = { "markdown" },
+      callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
+      end,
+    })
   end,
 }

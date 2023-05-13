837bddf969f9e81ede6762bbd00b0443f610654b
Simon Weiß <simon.weiss@alexanderthamm.com>
2023-05-13 15:33:50 +0200

    Configure rust-tools with communiyt pack

--- nvim/.config/AstroNvim/lua/user/plugins/community.lua
+++ nvim/.config/AstroNvim/lua/user/plugins/community.lua
@@ -1,7 +1,50 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.rust", enabled = true },
+  {
+    "rust-tools.nvim",
+    opts = {
+      tools = {
+        -- rust-tools options
+        reload_workspace_from_cargo_toml = true,
+        inlay_hints = {
+          auto = true,
+          only_current_line = false,
+          show_parameter_hints = true,
+          highlight = "Cursor",
+        },
+        hover_actions = {
+          border = {
+            { "╒", "FloatBorder" },
+            { "═", "FloatBorder" },
+            { "╕", "FloatBorder" },
+            { "│", "FloatBorder" },
+            { "╛", "FloatBorder" },
+            { "─", "FloatBorder" },
+            { "╘", "FloatBorder" },
+            { "│", "FloatBorder" },
+          },
+          auto_focus = true,
+        },
+      },
+      server = {
+        -- standalone file support
+        -- setting it to false may improve startup time
+        standalone = true,
+        settings = {
+          ["rust-analyzer"] = {
+            lens = {
+              enable = true,
+            },
+            check = {
+              command = "clippy",
+            },
+          },
+        },
+      }, -- rust-analyzer options
+    },
+  },
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
--- nvim/.config/AstroNvim/lua/user/plugins/core.lua
+++ nvim/.config/AstroNvim/lua/user/plugins/core.lua
@@ -37,21 +37,6 @@ return {
       },
     },
   },
-  lsp = {
-    setup_handlers = {
-      -- add custom handler
-      rust_analyzer = function(_, opts) require("rust-tools").setup { server = opts } end
-    },
-  },
-  plugins = {
-    "simrat39/rust-tools.nvim", -- add lsp plugin
-    {
-      "williamboman/mason-lspconfig.nvim",
-      opts = {
-        ensure_installed = { "rust_analyzer" },
-      },
-    },
-  },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --

9efcb78d632c8c72a2e6c81396607004d29d71dd
Simon Weiß <simon.weiss@alexanderthamm.com>
2023-05-13 15:34:02 +0200

    Ensure lsp installation

--- nvim/.config/AstroNvim/lua/user/plugins/mason.lua
+++ nvim/.config/AstroNvim/lua/user/plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "pyright", "docker_ls", "rust_analyzer", "yamlls", "jsonls", "ruff_lsp" },
+      ensure_installed = { "lua_ls", "pyright", "dockerls", "yamlls", "jsonls", "ruff_lsp" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources

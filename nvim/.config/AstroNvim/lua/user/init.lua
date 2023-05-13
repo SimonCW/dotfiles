e1bd6e876b0003d74617a49c47a38960e4b20450
Simon Weiß <simon.weiss@alexanderthamm.com>
2023-05-13 20:20:53 +0200

    Change colorscheme

--- nvim/.config/AstroNvim/lua/user/init.lua
+++ nvim/.config/AstroNvim/lua/user/init.lua
@@ -17,7 +17,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "catppuccin",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,

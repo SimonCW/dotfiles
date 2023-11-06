fd590e81dc9c81c5e64ee10febf59963d2c5e98d
Simon Weiß <weiss.simon@outlook.com>
2023-11-06 14:47:44 +0100

    Add treesitter installs

--- nvim/.config/AstroNvim/lua/user/plugins/treesitter.lua
+++ nvim/.config/AstroNvim/lua/user/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua", "rust", "python" },
+    ensure_installed = { "lua", "rust", "python", "toml", "json" },
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = false,

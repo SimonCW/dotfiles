83ab6264029a115388ebb3dcf0dd737a5690dbeb
Simon Weiß <weiss.simon@outlook.com>
2024-01-31 14:43:16 +0100

    Configure cmp to show snippets last

--- nvim/.config/AstroNvim/lua/user/init.lua
+++ nvim/.config/AstroNvim/lua/user/init.lua
@@ -61,6 +61,27 @@ return {
       },
     },
   },
+  plugins = {
+    { -- override nvim-cmp plugin
+      "hrsh7th/nvim-cmp",
+      -- override the options table that is used in the `require("cmp").setup()` call
+      opts = function(_, opts)
+        -- opts parameter is the default options table
+        -- the function is lazy loaded so cmp is able to be required
+        local cmp = require "cmp"
+        -- modify the sources part of the options table
+        opts.sources = cmp.config.sources {
+          { name = "nvim_lsp", priority = 1000 },
+          { name = "buffer", priority = 500 },
+          { name = "path", priority = 250 },
+          { name = "luasnip", priority = 100 },
+        }
+
+        -- return the new table to be used
+        return opts
+      end,
+    },
+  },
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here

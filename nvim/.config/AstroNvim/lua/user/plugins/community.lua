9d4b32086df52a3446884c066ea5cccc16da2fe7
Simon Weiß <weiss.simon@outlook.com>
2024-03-08 16:01:20 +0100

    Remove package with installation errors

--- nvim/.config/AstroNvim/lua/user/plugins/community.lua
+++ nvim/.config/AstroNvim/lua/user/plugins/community.lua
@@ -1,8 +1,8 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim", enabled = true },
-  { import = "astrocommunity.pack.rust", enabled = true },
+  --{ import = "astrocommunity.markdown-and-latex.markdown-preview-nvim", enabled = true },
+  { import = "astrocommunity.pack.rust",              enabled = true },
   {
     "rust-tools.nvim",
     opts = {

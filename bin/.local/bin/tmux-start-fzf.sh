7de0893da89c2e4e1058fbe37fe63af487ec701d
Simon Weiß <weiss.simon@outlook.com>
2024-04-17 12:49:30 +0200

    Improve search, filter errors

--- bin/.local/bin/tmux-start-fzf.sh
+++ bin/.local/bin/tmux-start-fzf.sh
@@ -5,7 +5,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-  selected=$(find ~/Repos/ ~/Repos/contrib/ ~/Repos/scratch ~/Repos/learning ~/Repos/hrsw-medtech -mindepth 1 -maxdepth 1 -type d | fzf)
+  selected=$(fd . ~/Repos/ ~/Repos/contrib/ ~/Repos/scratch ~/Repos/learning ~/Repos/teamcto --maxdepth 1 --type d 2>/dev/null | fzf)
 fi
 
 # Exit if nothing found

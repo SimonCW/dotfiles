c7e74a68c413fb9e47a330d40102d5fc3df2b315
Simon Weiß <weiss.simon@outlook.com>
2024-06-03 15:13:32 +0200

    Add bash and markdown support

--- nvim/.config/AstroNvim/lua/community.lua
+++ nvim/.config/AstroNvim/lua/community.lua
@@ -7,7 +7,9 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.lua", enabled = true },
+  { import = "astrocommunity.pack.bash", enabled = true },
+  { import = "astrocommunity.pack.markdown", enabled = true },
   -- import/override with your plugins folder
   { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
   {

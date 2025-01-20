fed2d81c07ba8f7fb996be89654aa11972d64b9c
Simon Weiß <weiss.simon@outlook.com>
2025-01-20 10:27:31 +0100

    Disable dap, I don't use it

---
+++ nvim/.config/AstroNvim/lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    enabled = false,
+  },
+  { "mfussenegger/nvim-dap", enabled = false },
+}
--- nvim/.config/AstroNvim/lua/plugins/mason.lua
+++ nvim/.config/AstroNvim/lua/plugins/mason.lua
@@ -36,6 +36,7 @@ return {
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
+    enabled = false,
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {

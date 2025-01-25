aa4b8c22f964a0f018a7423b9aa62197affc2f2d
Simon Weiß <weiss.simon@outlook.com>
2025-01-25 14:38:29 +0100

    Cleanup

--- nvim/.config/AstroNvim/lua/plugins/astrocore.lua
+++ nvim/.config/AstroNvim/lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -11,6 +9,12 @@ return {
   ---@type AstroCoreOpts
   opts = {
     autocmds = {
+      -- conceal_zero = {
+      --   desc = "Set conceallevel to 0 for file types like json and yaml. This allows to see quotation marks and such",
+      --   event = "FileType",
+      --   pattern = { "json", "yaml", "toml" },
+      --   callback = function() vim.opt_local.conceallevel = 0 end,
+      -- },
       textspellwrap = {
         {
           desc = "Enable spell and wrap for text documents",

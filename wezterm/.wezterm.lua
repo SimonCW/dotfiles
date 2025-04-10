b4f03efb4b0471ecae5445807e199bb8c27df788
Simon Weiß <weiss.simon@outlook.com>
2025-04-10 16:33:37 +0200

    Compose key when right alt

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,7 @@ local act = wezterm.action
 
 config.color_scheme = "Catppuccin Mocha" -- Latte, Frappé, Macchiato, Mocha
 config.font_size = 16
-config.font = wezterm.font({             -- Normal text
+config.font = wezterm.font({ -- Normal text
 	family = "Monaspace Neon",
 	weight = "Medium",
 	harfbuzz_features = { "calt", "liga" },
@@ -43,6 +43,7 @@ config.native_macos_fullscreen_mode = true
 config.pane_focus_follows_mouse = false
 -- Necessary for my Moonlander Layout to work.
 config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
 config.window_decorations = "TITLE | RESIZE"
 
 --------------------
@@ -89,15 +90,15 @@ end
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	-- splitting
-	{ mods = "LEADER", key = "h",       action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ mods = "LEADER", key = "v",       action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ mods = "LEADER", key = "x",       action = act.CloseCurrentPane({ confirm = false }) },
-	{ key = "f",       mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
-	{ key = "Enter",   mods = "LEADER", action = act.ActivateCopyMode },
-	{ key = "t",       mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-	{ key = "n",       mods = "LEADER", action = act.ActivateTabRelative(1) },
-	{ key = "p",       mods = "LEADER", action = act.ActivateTabRelative(-1) },
-	{ key = "w",       mods = "LEADER", action = act.ShowTabNavigator },
+	{ mods = "LEADER", key = "h", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ mods = "LEADER", key = "v", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ mods = "LEADER", key = "x", action = act.CloseCurrentPane({ confirm = false }) },
+	{ key = "f", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
+	{ key = "Enter", mods = "LEADER", action = act.ActivateCopyMode },
+	{ key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{ key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	{ key = "w", mods = "LEADER", action = act.ShowTabNavigator },
 
 	-- Navigation between panes and neovim using the smart-splits-function
 	-- move between split panes

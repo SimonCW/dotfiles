f33e16567d1f38b6b6f6b665d5a4ce666d411fb1
Simon Weiß <weiss.simon@outlook.com>
2025-04-10 13:46:29 +0200

    Migrate mason

--- nvim/.config/astronvim_v5/lua/plugins/mason.lua
+++ nvim/.config/astronvim_v5/lua/plugins/mason.lua
@@ -9,16 +9,15 @@ return {
     opts = {
       -- Make sure to use the names found in `:Mason`
       ensure_installed = {
-
-        -- "dockerls",
-        -- "yamlls",
-        -- "jsonls",
-        -- "taplo",
-
         -- install language servers
+        
         "lua-language-server",
-       "basedpyright", 
+        "basedpyright", 
         "markdown-oxide", -- lsp for working with obsidian files
+        "dockerfile-language-server",
+        "taplo",
+        "yaml-language-server",
+        "json-lsp",
 
         -- install formatters
         "stylua",

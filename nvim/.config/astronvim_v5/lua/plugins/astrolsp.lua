b84ca87fbb1bff8c107db25b30d97bdd05a177ac
Simon Weiß <weiss.simon@outlook.com>
2025-04-10 16:36:14 +0200

    Migrate lsp

--- nvim/.config/astronvim_v5/lua/plugins/astrolsp.lua
+++ nvim/.config/astronvim_v5/lua/plugins/astrolsp.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -13,7 +11,7 @@ return {
     -- Configuration table of features provided by AstroLSP
     features = {
       codelens = true, -- enable/disable codelens refresh on start
-      inlay_hints = false, -- enable/disable inlay hints on start
+      inlay_hints = true, -- enable/disable inlay hints on start
       semantic_tokens = true, -- enable/disable semantic token highlighting
     },
     -- customize lsp formatting options
@@ -25,6 +23,7 @@ return {
           -- "go",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
+          "php",
           -- "python",
         },
       },
@@ -39,12 +38,33 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
+      "rust_analyzer",
       -- "pyright"
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
+      markdown_oxide = { capabilities = { workspace = { didChangeWatchedFiles = { dynamicRegistration = true } } } },
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
+              extraArgs = { "--profile", "rust-analyzer" },
+            },
+            check = { command = "clippy" },
+            inlayHints = {
+              typeHints = { enable = true },
+              lifetimeElisionHints = { enable = "skip_trivial" },
+              bindingModeHints = { enable = true },
+              closingBraceHints = { enable = false },
+              closureCaptureHints = { enable = true },
+              parameterHints = { enable = false },
+              rangeExclusiveHints = { enable = true },
+            },
+          },
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {

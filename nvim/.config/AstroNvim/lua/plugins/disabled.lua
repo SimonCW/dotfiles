4dfb4444273f0eb083397d3673cf0651e8613a5d
Simon Weiß <weiss.simon@outlook.com>
2025-07-16 22:43:18 +0200

    Remove old config

--- nvim/.config/AstroNvim/lua/plugins/disabled.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    enabled = false,
-  },
-  { "mfussenegger/nvim-dap", enabled = false },
-  { "vuki656/package-info.nvim", enabled = false },
-}
